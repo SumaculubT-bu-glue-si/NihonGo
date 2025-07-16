5c4ac4ee9ff7b93ec7871f5ef77243e9b982d59a
TYR S. SUMACULUB <sumaculub_t@bu.glue-si.com>
2025-07-16 05:39:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/grammar-lessons/lessons-view.tsx
+++ src/app/grammar-lessons/lessons-view.tsx
@@ -39,6 +39,7 @@ import {
   AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
+  AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
 } from '@/components/ui/alert-dialog';
